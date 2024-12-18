3cad981c59c6280d52373bc9a8af2e10949b20e6
q6VQU77 <112760490+q6VQU77@users.noreply.github.com>
2024-12-18 12:26:13 -0600

    添加了 用户认证 功能

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  default = "example-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_name" {
+  default = "example-vm"
+}
+
+variable "vm_size" {
+  default = "Standard_B2s"
+}
+
+variable "admin_username" {
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  default = "ComplexPassword123!"
+}
